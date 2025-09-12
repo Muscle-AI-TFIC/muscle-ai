6a8327e0893f863816f02d6a9527c9ed558a4b75
Fuad Murad <murad.fuad24@gmail.com>
2025-09-12 14:24:41 -0300

    refactor: refactoring the code for test

--- src/muscle_ai/get_user.py
+++ src/muscle_ai/get_user.py
@@ -2,40 +2,48 @@ import firebase_admin
 from firebase_admin import credentials, auth
 import json
 
-cread_path = "C:/Users/murad/OneDrive/Documentos/muscle-ai/src/muscle_ai/key.json"
-output_file = "user.json"
+def get_user_firebase():
 
-try:
-    cred = credentials.Certificate(cread_path)
-    firebase_admin.initialize_app(cred)
+    if not firebase_admin._apps:
+        cred_path = "C:/Users/murad/OneDrive/Documentos/muscle-ai/src/muscle_ai/key.json"
+        cred = credentials.Certificate(cred_path)
+        firebase_admin.initialize_app(cred)
+ 
+    user_data= []
 
-    print("Verificando conexao")
+    for user in auth.list_users().iterate_all():
+        user_data.append({
+            'id': user.uid,
+            'email': user.email,
+            "name": user.display_name,
+            "active": user.disabled,
+        })
+    return user_data
 
-    users = auth.list_users()
+def save_user_json(user_data, filepath):
 
-    #print("\n Lista dos usuarios cadastrados:") // mexer para que ele mostre os dados para comparacao do json
+    with open(filepath, "w", encoding="utf-8") as file:
 
-    user_data = []
+        json.dump(user_data, file, indent=2, ensure_ascii=False)
+    print(f"As informações foram salvas com sucesso em {filepath}")
 
-    with open(output_file, "w", encoding="utf-8") as file:
-        for user in users.users:
-            user_data.append({
-                'id': user.uid,
-                'email' : user.email, 
-                "name" : user.display_name,  
-                "active": user.disabled,
-                })
+def main():
+    
+    cred_path = "C:/Users/murad/OneDrive/Documentos/muscle-ai/src/muscle_ai/key.json"
+    output_file = "user.json"
 
-        json.dump(user_data, file, indent= 2, ensure_ascii=False)
+    try:
+        cred = credentials.Certificate(cred_path)
+        firebase_admin.initialize_app(cred)
+        print("Verificando conexao")
 
-        
+        users = get_user_firebase()
+        save_user_json(users, output_file)
 
-    # Esta mensagem deve ser impressa SOMENTE depois que o loop terminar.
-    print(f"As informacoes foram salvas com sucesso em {output_file}")
+    except FileNotFoundError:
+        print(f"Erro: o arquivo com as credenciais {cred_path} não foi encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro inesperado: {e}")
 
-
-except FileNotFoundError:
-    print(f"Erro: o arquivo com as credenciais {cread_path} nao foi achado")
-
-except Exception as e:
-    print(f"Ocorreu um erro inesperado: {e}")
+if __name__ == "__main__":
+    main()
