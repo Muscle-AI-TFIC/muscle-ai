9a9811f26d57e55c976da08baba70050c4c719ee
CaioBonvicine <caiopbonvicine@gmail.com>
2025-10-24 22:50:42 -0300

    refactor: changing the ideia of trainer (not done yet)

--- src/muscle_ai/trainer.py
+++ src/muscle_ai/trainer.py
@@ -4,7 +4,7 @@ from muscle_ai.ai_client import ask_gemini
 
 
 def load_user_data(file_path: str) -> dict:
-    #Getting the users information from the json file
+
     path = Path(file_path)
     if not path.exists():
         raise FileNotFoundError(f"File {file_path} not found.")
@@ -13,16 +13,11 @@ def load_user_data(file_path: str) -> dict:
         return json.load(f)
 
 
-def generate_training_plan(user_data: dict) -> str:
-    #Sending the users information to the ai model to generate a training plan
+def generate_training_plan(traning_data: dict) -> str:
     prompt = (
         f"Create the best gym training plan for the following profile:\n\n"
-        f"Name: {user_data.get('name')}\n"
-        f"Age: {user_data.get('age')}\n"
-        f"Weight: {user_data.get('weight')} kg\n"
-        f"Height: {user_data.get('height')} m\n"
-        f"Goal: {user_data.get('objective')}\n"
-        f"Experience level: {user_data.get('level')}\n"
-        f"Preferred language: {user_data.get('language')}\n"
+        f"Age: {traning_data.get('idade')}\n"
+        f"Goal: {traning_data.get('objetivo')}\n"
+        f"Weight: {traning_data.get('peso')} kg\n"
     )
     return ask_gemini(prompt)
