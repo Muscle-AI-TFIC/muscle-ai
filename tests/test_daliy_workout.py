e8c401e3c325ed014822ccacd955c50745d9394b
Fuad Murad <murad.fuad24@gmail.com>
2025-10-30 20:41:17 -0300

    test: up tests to ask_gemini, daliy workout and daliy workout exercises

---
+++ tests/test_daliy_workout.py
@@ -0,0 +1,68 @@
+import pytest
+from unittest.mock import Mock, patch
+from muscle_ai.get_daily_workout import get_daily_workout_exercises
+from muscle_ai.get_daily_workout import get_daily_workout
+
+@patch("muscle_ai.get_daily_workout.initialize_supabase")
+def test_get_daily_workout_success(mock_initialize_supabase):
+
+    mock_supabase = Mock()
+    mock_response = Mock()
+
+    mock_data = [
+        {
+            "id": "12",
+            "user_id": "Fuad123",
+            "workout_date": "2024-01-15",
+        }
+    ]
+
+    mock_response.data = mock_data
+    mock_supabase.table.return_value.select.return_value.execute.return_value = mock_response
+
+    mock_initialize_supabase.return_value = mock_supabase
+
+    result = get_daily_workout()
+
+    expected = [
+        {
+            "id": "12",
+            "user_id": "Fuad123",
+            "workout_date": "2024-01-15",
+        }
+    ]
+
+    assert result == expected
+
+@patch("muscle_ai.get_daily_workout.initialize_supabase")
+def test_get_daily_workout_exercises_success(mock_initialize_supabase):
+
+    mock_supabase = Mock()
+    mock_response = Mock()
+
+    mock_data = [
+        {
+             "id" : "62" , 
+            "daily_workout_id": "19" , 
+            "exercise_id": "6"  , 
+        }
+    ]
+
+    mock_response.data = mock_data
+    mock_supabase.table.return_value.select.return_value.execute.return_value = mock_response
+
+    mock_initialize_supabase.return_value = mock_supabase
+
+    result = get_daily_workout_exercises()
+
+    expected = [
+        {
+             "id": "62", 
+            "daily_workout_id": "19", 
+            "exercise_id": "6", 
+        }
+    ]
+
+    assert result == expected
+
+
--- tests/trainer_test.py
+++ tests/trainer_test.py
@@ -5,7 +5,6 @@ from muscle_ai import trainer
 
 
 def test_generate_training_plan():
-    #Simulating the user data
     fake_user_data = {
         'name': 'Caio',
         'age': 25,
@@ -16,10 +15,8 @@ def test_generate_training_plan():
         'language': 'portuguese'
     }
 
-    #Simulating a fake response from the AI
     fake_response = "Fake traning"
 
-    #Mocking the ask_gemini function to return the fake response
     with patch("muscle_ai.trainer.ask_gemini", return_value=fake_response):
 
         result = trainer.generate_training_plan(fake_user_data)
@@ -28,7 +25,6 @@ def test_generate_training_plan():
 
 
 def test_load_user_data():
-    #Simulating JSON
     fake_json = json.dumps({
         'name': 'Caio',
         'age': 30,
@@ -39,15 +35,13 @@ def test_load_user_data():
         'language': 'portuguese'
     })
 
-    #Criating a mock to simulate the file
     mock_file = Mock()
     mock_file.__enter__ = Mock(return_value=mock_file)
     mock_file.__exit__ = Mock(return_value=None)
     mock_file.read = Mock(return_value=fake_json)
 
-    #Mocking the Path.exists method to always return True or the test will fail
     with patch("pathlib.Path.exists", return_value=True):
-        #Opening the mock file instead of a real file
+        
         with patch("builtins.open", return_value=mock_file):
             data = trainer.load_user_data("fake_path.json")
 
